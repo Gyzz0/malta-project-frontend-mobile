d114421e77b9b58a6100ae2db980425e20c463dc
Gaetano Izzo <gaetanoizzo2001@gmail.com>
2023-06-07 16:18:47 +0200

    Prima schermata

---
+++ Mobile.jsx
@@ -0,0 +1,11 @@
+import { Container } from 'react-bootstrap';
+
+export default function Mobile() {
+  return (
+    <>
+    <Container fluid>
+      <h1>Mobile</h1>
+    </Container>
+    </>
+  );
+}
